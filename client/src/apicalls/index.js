90584f3b5de5b154157ccab50df6b2cd1a5019a1
psaisuryatejareddy <saitejageneral@gmail.com>
2023-11-19 23:59:34 +0530

    uppp

--- client/src/apicalls/index.js
+++ client/src/apicalls/index.js
@@ -4,6 +4,9 @@ const backendURL = 'https://quizapplication-akd7.onrender.com/';
 
 const axiosInstance = axios.create({
   baseURL: backendURL,
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem('token')}`,
+  },
 });
 
 export default axiosInstance;
