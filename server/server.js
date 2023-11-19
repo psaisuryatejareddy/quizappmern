75e68b4f97af82bcd444fea4bd679cb33d1de411
psaisuryatejareddy <saitejageneral@gmail.com>
2023-11-19 23:19:41 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 require("dotenv").config();
 app.use(express.json());
@@ -6,25 +7,26 @@ const dbConfig = require("./config/dbConfig");
 
 const usersRoute = require("./routes/usersRoute");
 const examsRoute = require("./routes/examsRoute");
-const resportsRoute = require("./routes/reportsRoute");
+const reportsRoute = require("./routes/reportsRoute");
 
-
+// Enable CORS for all routes
+app.use(cors());
 
 app.use("/api/users", usersRoute);
 app.use("/api/exams", examsRoute);
-app.use("/api/reports", resportsRoute);
+app.use("/api/reports", reportsRoute);
+
 const port = process.env.PORT || 5000;
 
 const path = require("path");
 __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client" , "build")));
+  app.use(express.static(path.join(__dirname, "client", "build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });   
-} 
-
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
